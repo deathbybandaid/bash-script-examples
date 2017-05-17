1807b733259b328ad52c5bba409b3ccf13de14a1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-17 09:16:48 -0400

    Create bashincolor.sh

---
+++ bashincolor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+red='\e[1;31m%s\e[0m\n'
+green='\e[1;32m%s\e[0m\n'
+yellow='\e[1;33m%s\e[0m\n'
+blue='\e[1;34m%s\e[0m\n'
+magenta='\e[1;35m%s\e[0m\n'
+cyan='\e[1;36m%s\e[0m\n'
+
+printf "$green"   "This is a test in green"
+printf "$red"     "This is a test in red"
+printf "$yellow"  "This is a test in yellow"
+printf "$blue"    "This is a test in blue"
+printf "$magenta" "This is a test in magenta"
+printf "$cyan"    "This is a test in cyan"
